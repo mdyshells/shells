a983e42f89b3cdd14f5225d2f347d7866e663c38
mdyshells <126294216+mdyshells@users.noreply.github.com>
2023-02-27 10:59:37 -0800

    Delete _app directory

--- _app/immutable/chunks/1.437fa06c.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e6dec2ad.mjs";export{t as component};
--- _app/immutable/chunks/2.bb4d6ec8.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.c1c6b52e.mjs";export{t as component};
